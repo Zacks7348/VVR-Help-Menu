aae4d3987d7e5e649466bee2cc9b12ff29fe096b
Zacks7348 <zacks7348@gmail.com>
2021-03-22 21:35:32 -0400

    initial commit

---
+++ Assets/HelpWiindowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpWiindowScript : MonoBehaviour
+{
+    //Show Help Window
+    public void Show(Animator a)
+    {
+        a.SetBool("visible", true);
+    }
+
+    //Hide Help Window
+    public void Hide(Animator a)
+    {
+        a.SetBool("visible", false);
+    }
+        
+}
